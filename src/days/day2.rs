aca561e53c6840562ce457899775c077abc02bad
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2024-12-02 07:08:51 +0100

    Day 2, Star 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -17,31 +17,39 @@ fn get_input() -> Vec<Vec<usize>> {
         .collect()
 }
 
+fn is_safe(report: &[usize], ignore_report: Option<usize>) -> bool {
+    let mut previous_sign: Option<bool> = None;
+    let mut unsafe_report = false;
+    let mut filtered_report = report.to_owned();
+    if let Some(ignored) = ignore_report {
+        filtered_report.remove(ignored);
+    };
+    for i in 0..filtered_report.len() - 1 {
+        let (a, b) = (filtered_report[i], filtered_report[i + 1]);
+        let sign = a < b;
+        if let Some(is_increasing) = previous_sign {
+            if is_increasing != sign {
+                unsafe_report = true;
+                break;
+            }
+        } else {
+            previous_sign = Some(sign);
+        }
+
+        let step = a.abs_diff(b);
+        if !(1..=3).contains(&step) {
+            unsafe_report = true;
+            break;
+        }
+    }
+    !unsafe_report
+}
+
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let input = get_input();
     let mut safe_report_count = 0;
     for report in input {
-        let mut previous_sign: Option<bool> = None;
-        let mut unsafe_report = false;
-        for i in 0..report.len() - 1 {
-            let (a, b) = (report[i], report[i + 1]);
-            let sign = a < b;
-            if let Some(is_increasing) = previous_sign {
-                if is_increasing != sign {
-                    unsafe_report = true;
-                    break;
-                }
-            } else {
-                previous_sign = Some(sign);
-            }
-
-            let step = a.abs_diff(b);
-            if !(1..=3).contains(&step) {
-                unsafe_report = true;
-                break;
-            }
-        }
-        if !unsafe_report {
+        if is_safe(&report, None) {
             safe_report_count += 1;
         }
     }
@@ -50,5 +58,20 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let input = get_input();
+
+    let mut safe_report_count = 0;
+
+    for report in input {
+        if is_safe(&report, None) || (0..report.len()).any(|i| is_safe(&report, Some(i))) {
+            safe_report_count += 1;
+        }
+    }
+
+    println!(
+        "Taking the Problem Dampener into account, there are actually {} safe reports",
+        safe_report_count
+    );
+
+    Ok(())
 }
