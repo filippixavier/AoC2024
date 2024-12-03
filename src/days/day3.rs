dc379bb92457c16a51bcf896d2f07f5276cbd0df
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2024-12-03 07:41:40 +0100

    Day 3, Star 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -27,5 +27,29 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let instructions = get_input();
+    let correct_instruction =
+        Regex::new(r"(?:mul\((?<left>\d+),(?<right>\d+)\))|(?<yes>do\(\))|(?<no>don't\(\))")
+            .unwrap();
+    let mut activated = true;
+    let mut total = 0;
+
+    for cap in correct_instruction.captures_iter(&instructions) {
+        if cap.name("yes").is_some() {
+            activated = true;
+            continue;
+        }
+        if cap.name("no").is_some() {
+            activated = false;
+            continue;
+        }
+        if activated {
+            let (num1, num2) = (&cap["left"], &cap["right"]);
+            total += num1.parse::<usize>().unwrap() * num2.parse::<usize>().unwrap();
+        }
+    }
+
+    println!("Result of all added activated multiplications: {}", total);
+
+    Ok(())
 }
