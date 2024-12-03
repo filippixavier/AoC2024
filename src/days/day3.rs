733f77cd1816a095510f99ab4b3db27269e39b7a
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2024-12-03 07:30:13 +0100

    Day 3, Star 1

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,7 +1,29 @@
 use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+use regex::Regex;
+
+fn get_input() -> String {
+    fs::read_to_string(Path::new("./input/day3.input"))
+        .expect("Something went wrong with the input")
+}
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 1 not ready");
+    let instructions = get_input();
+    let correct_instruction = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut total = 0;
+
+    for (_, [num1, num2]) in correct_instruction
+        .captures_iter(&instructions)
+        .map(|c| c.extract())
+    {
+        total += num1.parse::<usize>().unwrap() * num2.parse::<usize>().unwrap();
+    }
+
+    println!("Result of all added valid multiplications: {}", total);
+
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
