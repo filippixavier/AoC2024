b9e800e0cd9bf0e8a34e6d15750842ba27d88df5
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2024-12-08 08:29:23 +0100

    Day 8, Star 1

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,7 +1,62 @@
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../../input/day8.input");
+
+fn get_input() -> Vec<Vec<char>> {
+    INPUT
+        .trim()
+        .lines()
+        .map(|line| line.trim().chars().collect())
+        .collect()
+}
+
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 1 not ready");
+    let radio_map = get_input();
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+    let bottom_right_coord = (radio_map.len(), radio_map[0].len());
+
+    for (i, map_line) in radio_map.iter().enumerate() {
+        for (j, map_val) in map_line.iter().enumerate() {
+            if map_val.is_ascii_alphanumeric() {
+                let coordinates = antennas.entry(*map_val).or_default();
+                coordinates.push((i, j));
+            }
+        }
+    }
+
+    for coordinates in antennas.values() {
+        for couple in coordinates.iter().combinations(2) {
+            let (left, right) = (couple[0].min(couple[1]), couple[0].max(couple[1]));
+            let (diff_x, diff_y) = (left.0.abs_diff(right.0), left.1.abs_diff(right.1));
+
+            println!("{:?} {:?} {:?}", left, right, (diff_x, diff_y));
+
+            if diff_x <= left.0 {
+                if left.1 <= right.1 && diff_y <= left.1 {
+                    antinodes.insert((left.0 - diff_x, left.1 - diff_y));
+                }
+                if left.1 > right.1 && left.1 + diff_y < bottom_right_coord.1 {
+                    antinodes.insert((left.0 - diff_x, left.1 + diff_y));
+                }
+            }
+            if right.0 + diff_x < bottom_right_coord.0 {
+                if right.1 <= left.1 && diff_y <= right.1 {
+                    antinodes.insert((right.0 + diff_x, right.1 - diff_y));
+                }
+                if right.1 > left.1 && right.1 + diff_y < bottom_right_coord.1 {
+                    antinodes.insert((right.0 + diff_x, right.1 + diff_y));
+                }
+            };
+        }
+    }
+
+    println!("There are {:?} antinodes within the map", antinodes.len());
+
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
