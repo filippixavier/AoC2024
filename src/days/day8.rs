3c4a05663cfa3d55a9506bcf1733dac5ee315d69
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2024-12-08 10:49:10 +0100

    Day 8, Star 2

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -13,6 +13,21 @@ fn get_input() -> Vec<Vec<char>> {
         .collect()
 }
 
+fn display_map(map: &[Vec<char>], antennas: &HashSet<(usize, usize)>) {
+    for (i, line) in map.iter().enumerate() {
+        for (j, ch) in line.iter().enumerate() {
+            if ch.is_ascii_alphanumeric() {
+                print!("{}", ch);
+            } else if antennas.contains(&(i, j)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let radio_map = get_input();
     let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
@@ -33,8 +48,6 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
             let (left, right) = (couple[0].min(couple[1]), couple[0].max(couple[1]));
             let (diff_x, diff_y) = (left.0.abs_diff(right.0), left.1.abs_diff(right.1));
 
-            println!("{:?} {:?} {:?}", left, right, (diff_x, diff_y));
-
             if diff_x <= left.0 {
                 if left.1 <= right.1 && diff_y <= left.1 {
                     antinodes.insert((left.0 - diff_x, left.1 - diff_y));
@@ -54,11 +67,65 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         }
     }
 
+    display_map(&radio_map, &antinodes);
+
     println!("There are {:?} antinodes within the map", antinodes.len());
 
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let radio_map = get_input();
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+    let bottom_right_coord = (radio_map.len(), radio_map[0].len());
+
+    for (i, map_line) in radio_map.iter().enumerate() {
+        for (j, map_val) in map_line.iter().enumerate() {
+            if map_val.is_ascii_alphanumeric() {
+                let coordinates = antennas.entry(*map_val).or_default();
+                coordinates.push((i, j));
+            }
+        }
+    }
+
+    for coordinates in antennas.values() {
+        for couple in coordinates.iter().combinations(2) {
+            let (left, right) = (couple[0], couple[1]);
+
+            let a: (isize, isize) = (
+                right.0 as isize - left.0 as isize,
+                right.1 as isize - left.1 as isize,
+            );
+
+            let b: (isize, isize) = (left.0 as isize * a.1 - a.0 * left.1 as isize, a.1);
+
+            if a.1 == 0 {
+                for y in 0..bottom_right_coord.0 {
+                    antinodes.insert((y, left.1));
+                }
+            } else {
+                for x in 0..bottom_right_coord.1 {
+                    let point = (a.0 * x as isize + b.0, a.1);
+                    if point.0 % point.1 == 0 {
+                        let y = (point.0 / point.1) as usize;
+                        if y < bottom_right_coord.0 {
+                            antinodes.insert((y, x));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!();
+
+    display_map(&radio_map, &antinodes);
+
+    println!(
+        "Counting harmonic resonances, there are {:?} antinodes within the map",
+        antinodes.len()
+    );
+
+    Ok(())
 }
