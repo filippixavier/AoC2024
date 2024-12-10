a64ced07d7c85278ba695349912f7ed4bbeb9a10
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2024-12-10 15:43:11 +0100

    Day 10, Star 2

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -20,20 +20,25 @@ fn hike(
     max: (usize, usize),
     current_pos: (usize, usize),
     reached: &mut HashSet<(usize, usize)>,
+    use_reached: bool,
 ) -> usize {
     let mut total = 0;
 
     let current_height = topographic_map[current_pos.0][current_pos.1];
 
     if current_height == 9 {
-        return if reached.insert(current_pos) { 1 } else { 0 };
+        return if !use_reached || reached.insert(current_pos) {
+            1
+        } else {
+            0
+        };
     }
 
     if current_pos.0 > 0 {
         let next_pos = (current_pos.0 - 1, current_pos.1);
         let next_height = topographic_map[next_pos.0][next_pos.1];
         if current_height + 1 == next_height {
-            total += hike(topographic_map, max, next_pos, reached);
+            total += hike(topographic_map, max, next_pos, reached, use_reached);
         }
     }
 
@@ -41,7 +46,7 @@ fn hike(
         let next_pos = (current_pos.0 + 1, current_pos.1);
         let next_height = topographic_map[next_pos.0][next_pos.1];
         if current_height + 1 == next_height {
-            total += hike(topographic_map, max, next_pos, reached);
+            total += hike(topographic_map, max, next_pos, reached, use_reached);
         }
     }
 
@@ -49,7 +54,7 @@ fn hike(
         let next_pos = (current_pos.0, current_pos.1 - 1);
         let next_height = topographic_map[next_pos.0][next_pos.1];
         if current_height + 1 == next_height {
-            total += hike(topographic_map, max, next_pos, reached);
+            total += hike(topographic_map, max, next_pos, reached, use_reached);
         }
     }
 
@@ -57,7 +62,7 @@ fn hike(
         let next_pos = (current_pos.0, current_pos.1 + 1);
         let next_height = topographic_map[next_pos.0][next_pos.1];
         if current_height + 1 == next_height {
-            total += hike(topographic_map, max, next_pos, reached);
+            total += hike(topographic_map, max, next_pos, reached, use_reached);
         }
     }
 
@@ -82,13 +87,13 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         .collect();
 
     for (line, col) in starting_points {
-        let hike_total = hike(
+        score += hike(
             &topographic_map,
             (max_line, max_col),
             (line, col),
             &mut HashSet::new(),
+            true,
         );
-        score += hike_total;
     }
 
     println!("The sum of score of all trailheads is {}", score);
@@ -97,5 +102,33 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let topographic_map = get_input();
+    let (max_line, max_col) = (topographic_map.len() - 1, topographic_map[0].len() - 1);
+    let mut score = 0;
+
+    let starting_points: Vec<(usize, usize)> = topographic_map
+        .iter()
+        .enumerate()
+        .flat_map(|(line_no, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, val)| **val == 0)
+                .map(|(col_no, _)| (line_no, col_no))
+                .collect::<Vec<(usize, usize)>>()
+        })
+        .collect();
+
+    for (line, col) in starting_points {
+        score += hike(
+            &topographic_map,
+            (max_line, max_col),
+            (line, col),
+            &mut HashSet::new(),
+            false,
+        );
+    }
+
+    println!("The sum of ratings of all trailheads is {}", score);
+
+    Ok(())
 }
